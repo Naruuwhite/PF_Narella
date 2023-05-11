cf2190c4651e58ace9e964e5ee297251cfaa332a
Naruuwhite <122284808+Naruuwhite@users.noreply.github.com>
2023-05-10 22:03:02 -0300

    Creación de carpeta FRONTEND

---
+++ PF_Narella/FrontEnd/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardSoftSComponent } from './components/hard-soft-s/hard-soft-s.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardSoftSComponent,
+    ProyectosComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
