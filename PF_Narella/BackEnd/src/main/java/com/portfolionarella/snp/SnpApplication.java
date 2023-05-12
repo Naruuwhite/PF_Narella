6deaab8e9b358011c2090f2b3ba0646d2a778fc7
Naruuwhite <122284808+Naruuwhite@users.noreply.github.com>
2023-05-11 21:40:44 -0300

    Creacion de carpeta BackEnd
    Corrección de errores dentro de la página y creación de carpeta BackEnd

---
+++ PF_Narella/BackEnd/src/main/java/com/portfolionarella/snp/SnpApplication.java
@@ -0,0 +1,13 @@
+package com.portfolionarella.snp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnpApplication.class, args);
+	}
+
+}
---
+++ PF_Narella/BackEnd/src/test/java/com/portfolionarella/snp/SnpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolionarella.snp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
